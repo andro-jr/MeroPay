2bcbea893a2fa9edc79630d7f2b0df79861d547a
prabin <prabin20panta@gmail.com>
2023-08-06 11:23:45 +0545

    authentication on home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,20 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthProvider';
 import { Navigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const { authInfo } = useContext(AuthContext);
+  const { isLoggedIn } = authInfo;
+
+  useEffect(() => {
+    if (!isLoggedIn) navigate('/auth/sign-in');
+  }, [isLoggedIn]);
+
   return (
     <div>
-      <Link to='/auth/signup'>Signup</Link>
+      <Link to='/auth/sign-up'>Signup</Link>
     </div>
   );
 };
