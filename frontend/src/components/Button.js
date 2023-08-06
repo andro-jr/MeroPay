5ccf81dc1ab14846bc8dd10951d715ac58d94c0c
prabin <prabin20panta@gmail.com>
2023-08-06 12:10:14 +0545

    button design

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const Button = ({ title, styles }) => {
   return (
-    <button className={`px-8 py-3 rounded-md w-full mt-8 ${styles}`}>
-      {title}
+    <button
+      className={`px-8 py-3 rounded-md w-full mt-8 ${styles} custom_primary_button`}
+    >
+      <div className='button_text'>{title}</div>
     </button>
   );
 };
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthProvider';
-import { Navigate } from 'react-router-dom';
 
 const Home = () => {
   const navigate = useNavigate();
