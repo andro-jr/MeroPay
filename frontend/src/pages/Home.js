048ccebf553cdc4ce87ba49ea48543ccfd2e7922
prabin <prabin20panta@gmail.com>
2023-08-06 10:09:01 +0545

    minor bug fix - auth()

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,12 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import { AuthContext } from "../context/AuthProvider";
-import { Navigate } from "react-router-dom";
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthProvider';
+import { Navigate } from 'react-router-dom';
 
 const Home = () => {
-  const {isAuth} = useContext(AuthContext);
-
-  isAuth();
-  
-
-
   return (
     <div>
-      <Link to="/auth/signup">Signup</Link>
+      <Link to='/auth/signup'>Signup</Link>
     </div>
   );
 };
