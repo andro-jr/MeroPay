b4f50ee8eac8a2e6e16c819d451e8070c1161916
prabin <prabin20panta@gmail.com>
2023-08-08 23:17:06 +0545

    resend email verification part completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import EmailVerification from "./pages/EmailVerification";
-import ForgetPassword from "./pages/ForgetPassword";
-import ResetPassword from "./pages/ResetPassword";
-import NotFound from "./pages/NotFound";
-import VerifyUser from "./pages/VerifyUser";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import EmailVerification from './pages/EmailVerification';
+import ForgetPassword from './pages/ForgetPassword';
+import ResetPassword from './pages/ResetPassword';
+import NotFound from './pages/NotFound';
+import VerifyUser from './pages/VerifyUser';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="*" element={<NotFound />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/auth/verify-user" element={<VerifyUser />} />
-      <Route path="/auth/sign-in" element={<Login />} />
-      <Route path="/auth/signup" element={<Signup />} />
-      <Route path="/auth/about" element={<About />} />
-      <Route path="/auth/forget-password" element={<ForgetPassword />} />
-      <Route path="/auth/reset-password" element={<ResetPassword />} />
-      <Route path="/auth/OtpVerification" element={<EmailVerification />} />
+      <Route path='*' element={<NotFound />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/auth/sign-in' element={<Login />} />
+      <Route path='/auth/signup' element={<Signup />} />
+      <Route path='/auth/about' element={<About />} />
+      <Route path='/auth/forget-password' element={<ForgetPassword />} />
+      <Route path='/auth/reset-password' element={<ResetPassword />} />
+      <Route path='/auth/OtpVerification' element={<EmailVerification />} />
     </Routes>
   );
 };
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,38 +1,26 @@
-import { Navigate } from "react-router-dom";
-
-import React, { useContext, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthProvider";
-// import { resendEmailVerificationToken } from "../api/auth";
-import { NotificationContext } from "../context/NotificationProvider";
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthProvider';
+import { NotificationContext } from '../context/NotificationProvider';
+import VerifyUser from './VerifyUser';
 
 const Home = () => {
   const navigate = useNavigate();
   const { authInfo, handleLogout } = useContext(AuthContext);
-  // const { updateNotification } = useContext(NotificationContext);
   const { isLoggedIn } = authInfo;
   const isVerified = authInfo.profile?.isVerified;
-  // const id = authInfo.profile?.id;
-  // console.log(id);
-  // console.log(authInfo);
-
-  console.log(authInfo);
-
-  // const isVerified = authInfo.profile?.isVerified;
-  console.log(isVerified);
 
   useEffect(() => {
-    if (!isLoggedIn) navigate("/auth/sign-in");
-    else if (isLoggedIn && !isVerified) {
-      console.log(authInfo);
-      console.log(isVerified);
-      navigate("/auth/verify-user");
-    }
-  }, [isLoggedIn, isVerified]);
+    if (!isLoggedIn) navigate('/auth/sign-in');
+  }, [isLoggedIn]);
+
+  if (!isVerified) {
+    return <VerifyUser />;
+  }
 
   return (
     <div>
-      <Link to="/auth/signup">Signup</Link>
+      <Link to='/auth/signup'>Signup</Link>
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
