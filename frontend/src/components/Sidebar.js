1e3040eeb567c85eef311c0e6e4677936d5ab2bf
prabin <prabin20panta@gmail.com>
2023-08-09 23:55:18 +0545

    pending loader on sign in

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import Logout from "./Logout";
-import DashboardOption from "./DashboardOption";
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Logout from './Logout';
+import DashboardOption from './DashboardOption';
+import { AuthContext } from '../context/AuthProvider';
 
 const Sidebar = () => {
   const [active, setActive] = useState(true);
+  const { authInfo } = useContext(AuthContext);
+
+  const username = authInfo.profile?.name;
+  const useremail = authInfo.profile?.email;
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -13,26 +18,26 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="sidebar flex flex-col gap-4">
-      <div className="bg-white rounded-xl">
-        <div className="profile">
-          <div className="profile-avatar">
-            <Link to="/">
-              <img src="/avatar.jpg" alt="profile-avatar" />
+    <div className='sidebar flex flex-col gap-4'>
+      <div className='bg-white rounded-xl'>
+        <div className='profile'>
+          <div className='profile-avatar'>
+            <Link to='/'>
+              <img src='/avatar.jpg' alt='profile-avatar' />
             </Link>
           </div>
-          <div className="avatar-info">
+          <div className='avatar-info'>
             <p>
-              <Link to="/">leon lama</Link>
+              <Link to='/'>{username}</Link>
             </p>
             <span>
-              <Link to="/">email@email.com</Link>
+              <Link to='/'>{useremail}</Link>
             </span>
           </div>
         </div>
       </div>
-      <div className="flex flex-col justify-between min-h-[60vh]">
-        <div className="dashboard bg-white rounded-xl">
+      <div className='flex flex-col justify-between min-h-[60vh]'>
+        <div className='dashboard bg-white rounded-xl'>
           <DashboardOption />
         </div>
         <div>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,32 +1,34 @@
-import React, { useContext, useEffect, useState } from "react";
-import FormInput from "../components/Form/FormInput";
-import Button from "../components/Button";
-import { Link, useNavigate } from "react-router-dom";
-import FormSideDetails from "../components/Form/FormSideDetails";
-import { AuthContext } from "../context/AuthProvider";
-import { NotificationContext } from "../context/NotificationProvider";
-import Container from "../components/Container";
+import React, { useContext, useEffect, useState } from 'react';
+import FormInput from '../components/Form/FormInput';
+import Button from '../components/Button';
+import { Link, useNavigate } from 'react-router-dom';
+import FormSideDetails from '../components/Form/FormSideDetails';
+import { AuthContext } from '../context/AuthProvider';
+import { NotificationContext } from '../context/NotificationProvider';
+import Container from '../components/Container';
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const { authInfo, handleLogin } = useContext(AuthContext);
+  const { authInfo, handleLogin, isPending } = useContext(AuthContext);
   const { isLoggedIn } = authInfo;
+
+  const pending = authInfo.isPending;
 
   const { updateNotification } = useContext(NotificationContext);
 
   const [inputData, setInputData] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
   const validateUserInput = (userInput) => {
     const { email, password } = userInput;
     const isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (!email.trim()) return { ok: false, err: "Email is missing" };
+    if (!email.trim()) return { ok: false, err: 'Email is missing' };
     if (!isValidEmail.test(email))
-      return { ok: false, err: "Email is invalid" };
+      return { ok: false, err: 'Email is invalid' };
 
     return { ok: true };
   };
@@ -43,8 +45,8 @@ const Login = () => {
     e.preventDefault();
 
     const { ok, err } = validateUserInput(inputData);
-    
-    if (err) return updateNotification("error", err);
+
+    if (err) return updateNotification('error', err);
 
     const payload = {
       email: inputData.email,
@@ -55,52 +57,57 @@ const Login = () => {
   };
 
   useEffect(() => {
-    if (isLoggedIn) navigate("/");
+    if (isLoggedIn) navigate('/');
   }, [isLoggedIn]);
 
   return (
     <Container>
-      <div className="container max-w-[1200px] m-auto login min-h-[70vh]">
+      {pending && (
+        <div className='fixed w-full h-screen backdrop-blur-md border-2 z-10'>
+          <img src='/paper-plane.gif' alt='' className='h-full ' />
+        </div>
+      )}
+      <div className='container max-w-[1200px] m-auto login min-h-[70vh]'>
         <FormSideDetails />
 
-        <div className="flex flex-col items-center my-auto pl:0 xl:pl-20">
-          <h3 className="text-4xl text-blue-600 font-bold mt-0 md:mt-10">
+        <div className='flex flex-col items-center my-auto pl:0 xl:pl-20'>
+          <h3 className='text-4xl text-blue-600 font-bold mt-0 md:mt-10'>
             Log in to Mero Pay
           </h3>
-          <p className="mb-8 mt-4 max-w-[350px] text-center">
+          <p className='mb-8 mt-4 max-w-[350px] text-center'>
             Welcome to Mero Pay. Your complete expense Tracker
           </p>
           <form onSubmit={handleSubmit}>
             <FormInput
-              placeholder="abc@example.com"
-              type="text"
-              name="email"
+              placeholder='abc@example.com'
+              type='text'
+              name='email'
               value={inputData}
               onChange={handleChange}
             />
             <FormInput
-              placeholder="************"
-              type="password"
-              name="password"
+              placeholder='************'
+              type='password'
+              name='password'
               value={inputData}
               onChange={handleChange}
             />
-            <span className="text-sm first-letter text-gray-400 hover:text-blue-600 transition-all duration-0 hover:duration-50 ease-in-out hover:underline">
-              <Link to="/auth/forget-password">Forgot password?</Link>
+            <span className='text-sm first-letter text-gray-400 hover:text-blue-600 transition-all duration-0 hover:duration-50 ease-in-out hover:underline'>
+              <Link to='/auth/forget-password'>Forgot password?</Link>
             </span>
             <Button
-              title="Login"
-              styles="bg-blue-600 text-white hover:bg-blue-700 transition-all duration-0 hover:duration-150 ease-in-out"
+              title='Login'
+              styles='bg-blue-600 text-white hover:bg-blue-700 transition-all duration-0 hover:duration-150 ease-in-out'
             />
 
             {/* <span>
               New to MeroPay?<Link to="/register">Create Account</Link>
             </span> */}
-            <span className="text-sm text-gray-400 flex gap-2 mt-14 text-center items-center justify-center">
+            <span className='text-sm text-gray-400 flex gap-2 mt-14 text-center items-center justify-center'>
               <p>New to MeroPay?</p>
               <Link
-                to="/auth/signup"
-                className=" hover:text-blue-600 transition-all duration-0 hover:duration-50 ease-in-out hover:underline"
+                to='/auth/signup'
+                className=' hover:text-blue-600 transition-all duration-0 hover:duration-50 ease-in-out hover:underline'
               >
                 Create Account
               </Link>
