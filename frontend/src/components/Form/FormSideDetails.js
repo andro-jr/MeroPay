72619fc49b88d4155e7ffea4a3161a797a26d3f6
prabin <prabin20panta@gmail.com>
2023-08-10 10:10:42 +0545

    auth form side border managed

--- frontend/src/components/Form/FormSideDetails.js
+++ frontend/src/components/Form/FormSideDetails.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
 const FormSideDetails = () => {
   return (
     <div>
-      <div className="hidden lg:flex lg:p-0 xl:p-10 gradient__border items-center flex-col">
-        <img src="/login.png" alt="online expense" />
-        <h3 className="text-4xl text-blue-600 font-bold">Mero Pay</h3>
-        <p className="text-center max-w-[600px] mt-2">
+      <div className='hidden lg:flex lg:p-0 xl:p-10 side__border items-center flex-col'>
+        <img src='/login.png' alt='online expense' />
+        <h3 className='text-4xl text-blue-600 font-bold'>Mero Pay</h3>
+        <p className='text-center max-w-[600px] mt-2'>
           Your seamless expense tracking companion for effortless financial
           management on-the-go.
         </p>
